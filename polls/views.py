3a6b159ade18c90b2c591adc4c529de09d3426e7
Kaleem Ullah <kaleemullah360@live.com>
2019-06-19 16:11:36 +0500

    polls-views

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template.defaultfilters import pprint
-import json
-from django.core import serializers
+from django.http.response import JsonResponse
+from django.utils import timezone
+from polls.models import Choice, Question  # Import the model classes we just 
 # Create your views here.
 
 def index(request):
-	return HttpResponse(pprint(dir(request)), content_type='application/json') 
+	json_r = build_index_data(request)
+	return JsonResponse(json_r)
+
+def index_api(request):
+	json_r = build_index_data(request)
+	return JsonResponse(json_r)
+
+def build_index_data(request):
+	questions = Question.objects.filter(question_text__contains="new", pub_date__lte=timezone.now())
+	q_1 = Question.objects.filter(id=1)
+	q_1 = q_1.first()
+	q_1_c=q_1.choice_set.create(choice_text='Not listed', votes=0)
+	q_1 = q_1.choice_set.all()
+	q_1_c.delete()
+
+	current_year = timezone.now().year
+	questions.filter(pub_date__year=current_year)
+	json_r = {}
+	if questions.count() > 0:
+		json_r['status']= '1'
+		json_r['timestamp']= timezone.now()
+		json_r['response']= list(questions.values())
+		json_r['choices']= list(q_1.values())
+		json_r['message'] = '%d records found' % (questions.count())
+	else:
+		json_r['status']= '0'
+		json_r['timestamp']= timezone.now()
+		json_r['response']= []
+		json_r['choices']= []
+		json_r['message'] = '%d records found' % (questions.count())
+	return json_r
--- weblog/views.py
+++ weblog/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http.response import JsonResponse
 from weblog.models import Author, Blog, Entry
 # Create your views here.
 
 def index(request):
-	return Author.objects.all()
+	all_authors = Author.objects.all()
+	json_r = {}
+	if all_authors.count() > 0:
+		json_r['status']= '1'
+		json_r['timestamp']= timezone.now()
+		json_r['message'] = '%d records found' % (all_authors.count())
+		json_r['response']= list(all_authors.values())
+	else:
+		json_r['status']= '0'
+		json_r['timestamp']= timezone.now()
+		json_r['message'] = '%d records found' % (all_authors.count())
+		json_r['response']= []
+
+	return JsonResponse(json_r)
+
+def index_api(request):
+	all_authors = Author.objects.all()
+	json_r = {}
+	if all_authors.count() > 0:
+		json_r['status']= '1'
+		json_r['timestamp']= timezone.now()
+		json_r['message'] = '%d records found' % (all_authors.count())
+		json_r['response']= list(all_authors.values())
+	else:
+		json_r['status']= '0'
+		json_r['timestamp']= timezone.now()
+		json_r['message'] = '%d records found' % (all_authors.count())
+		json_r['response']= []
+
+	return JsonResponse(json_r)
